a89dfbc99fa42789038c9dbb479b5cab53f15a1a
theHirsch <max@rose-niestetal.de>
2022-08-08 09:10:02 +0200

    style und AUfteilung

--- src/App.js
+++ src/App.js
@@ -6,32 +6,12 @@ import {DayPilot, DayPilotScheduler, DayPilotCalendar} from "@daypilot/daypilot-
 import Header from "./components/Header";
 import Bottom from "./components/Bottom.js";
 import "./App.css";
-import Button from '@mui/material/Button';
-import DeleteIcon from '@mui/icons-material/Delete';
-import SendIcon from '@mui/icons-material/Send';
-import TextField from '@mui/material/TextField';
-import { styled } from '@mui/material/styles';
-import { green } from '@mui/material/colors';
-import { red } from '@mui/material/colors';
+import "./index.css"
 import Stack from '@mui/material/Stack';
 import qs from 'qs';
 
 
-const SendButton = styled(Button)(({ theme }) => ({
-  color: theme.palette.getContrastText(green[500]),
-  backgroundColor: green[500],
-  '&:hover': {
-    backgroundColor: green[700],
-  },
-}));
 
-//ResetButton color
-const ResetButton = styled(Button)(({ theme }) => ({
-  color: theme.palette.getContrastText(red[500]),
-  '&:hover': {
-    backgroundColor: red[700],
-  },
-}));
 
 
 class App extends Component {
@@ -86,26 +66,10 @@ class App extends Component {
     return (
       <main>
       <Header />
-      <div id="calendar">
+ { /* <div id="calendar">
         <DayPilotCalendar {...config} ref={component => { this.calendar = component && component.control; }}/>
-        <TextField
-          required
-          id="filled-required"
-          label="Required"
-          defaultValue="Hello World"
-          variant="filled"
-        />
-        <TextField
-          error
-          id="outlined-error-helper-text"
-          label="Error"
-          defaultValue="Hello World"
-          helperText="Incorrect entry."
-        />
-        <ResetButton variant="secondary" size="large" startIcon={<DeleteIcon />}>Reset</ResetButton>
-        <SendButton variant="contained" size="large" endIcon={<SendIcon />}>Send</SendButton>
-      </div>
-{/*       <Bottom /> */ }
+      </div>} */ }
+      <Bottom />
       </main>
     );
   }
--- src/components/Bottom.js
+++ src/components/Bottom.js
@@ -1,24 +1,39 @@
 import React from "react";
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import DeleteIcon from '@mui/icons-material/Delete';
+import SendIcon from '@mui/icons-material/Send';
+import { styled } from '@mui/material/styles';
+import { green } from '@mui/material/colors';
+import { red } from '@mui/material/colors';
 
+// SendButton color
+const SendButton = styled(Button)(({ theme }) => ({
+    color: theme.palette.getContrastText(green[500]),
+    backgroundColor: green[500],
+    '&:hover': {
+      backgroundColor: green[700],
+    },
+  }));
+  
+//ResetButton color
+  const ResetButton = styled(Button)(({ theme }) => ({
+    color: theme.palette.getContrastText(red[500]),
+    '&:hover': {
+      backgroundColor: red[700],
+    },
+  }));
 export default function Bottom(props) {
   return (
-      <div className="container" id="">
-        <div id="Bottom">
-          <button
-            className="btn btn-outline-danger"
-            id="reset_view"
-            onClick={props.deleteAll}
-          >
-            Reset
-          </button>
-          <button
-            className="btn btn-outline-success"
-            id="send_request"
-            onClick={(e) => props.addNoteUI(e)}
-          >
-            Absenden
-          </button>
-        </div>
-      </div>
+    <body>
+    <div id="textfield">
+    <TextField required id="filled-required" label="Required" defaultValue="Hello World" variant="filled"/>
+    <TextField error id="outlined-error-helper-text" label="Error" defaultValue="Hello World" helperText="Incorrect entry."/>
+    </div>
+    <div id="buttons">
+    <ResetButton variant="secondary" size="large" startIcon={<DeleteIcon />}>Reset</ResetButton>
+    <SendButton variant="contained" size="large" endIcon={<SendIcon />}>Send</SendButton>
+    </div>
+    </body>
   );
 }
