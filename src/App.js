21fea72bf75c684b3e505eb9bd8c0012a70947aa
theHirsch <max@rose-niestetal.de>
2022-08-13 07:02:40 +0200

    two more comments

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Admin from "./views/Admin";
 import Schedule from "./views/Schedule";
 
+// URL/Path is settled here to APP (used in index.js), so we use the "localhost:3000" for the Schedule View and "localhost:3000/admin" for the Admin View 
 export default function App() {
   return (
     <div className="container mx-auto">
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+// App (App.js) is loaded here, so that file will get loaded with "npm start"
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
